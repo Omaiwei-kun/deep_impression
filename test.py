7017f3e8bfdbe0b5afb18b5dbd0f33b2d91eb19c
Samir55 <discere044@icloud.com>
2019-02-05 05:12:02 +0200

    Add video files.

--- test.py
+++ test.py
@@ -2,7 +2,7 @@ from util import load_model, predict_trait
 model = load_model()
 
 # data can be the path of any video that can be loaded by librosa and skvideo.
-data = '_fJcW5234bY.001.mp4'
+data = 'eI_7SimPnnQ.001.mp4'
 y = predict_trait(data, model)
 
 print(data)
