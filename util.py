3a4e3719986d371ad2ccdd83c9d79da72867d80b
Samir55 <discere044@icloud.com>
2019-02-05 03:13:20 +0200

    Make it chainer V5 and Python 3.6 compatible

--- util.py
+++ util.py
@@ -4,34 +4,31 @@ import librosa
 import numpy
 import skvideo.io
 
+
 def load_audio(data):
     return librosa.load(data, 16000)[0][None, None, None, :]
 
+
 def load_model():
     model = audiovisual_stream.ResNet18().to_gpu()
-    
+
     chainer.serializers.load_npz('./model', model)
-    
+
     return model
 
+
 def load_video(data):
-    videoCapture = skvideo.io.VideoCapture(data, (456, 256))
-    
-    videoCapture.open()
-    
+    videoCapture = skvideo.io.vreader(data,num_frames=27)
+
     x = []
-    
-    while True:
-        retval, image = videoCapture.read()
-        
-        if retval:
-            x.append(numpy.rollaxis(image, 2))
-        else:
-            break
-    
+
+    for image in videoCapture:
+        x.append(numpy.rollaxis(image, 2))
+
     return numpy.array(x, 'float32')
+
 
 def predict_trait(data, model):
     x = [load_audio(data), load_video(data)]
-    
+
     return model(x)
